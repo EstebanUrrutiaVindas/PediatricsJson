9447256b2796dadb32c5f1ba6d38d678556d7818
Urrutia0405 <esteban.urrutia.vindas.bh@gmail.com>
2025-01-26 19:17:22 -0600

    Add specialPermissions

--- Permissions.js
+++ Permissions.js
@@ -3,28 +3,25 @@ export const Permissions = {
     "ebd71b1e-8ac8-4a09-8b97-3848d93eba71": [
         { name: 'MESSAGES_RECEIVED', value: false }, //AGREGAR TRADUCCIÓN MENSAJES RECIBIDOS
         { name: 'UPCOMING_BIRTHDAYS', value: false}, //AGREGAR TRADUCCIÓN PRÓXIMOS CUMPLEAÑOS
-        { name: 'APPOINTMENTS', value: false},
-        { name: 'ACCESS_HOME', value: false}]
+        { name: 'APPOINTMENTS', value: false}]
     ,
     //CHILDREN
     "081e837d-88e5-4b33-a810-bd505df6e78c": [
-        { name: 'CREATE_RECORD', value: false },
+        { name: 'CREATE', value: false },
         { name: 'ACCESS_RECORD', value: false }, //AGREGAR TRADUCCIÓN ACCEDER A EXPEDIENTE
-        { name: 'UPDATE_RECORD', value: false },//AGREGAR TRADUCCIÓN ACTUALIZAR EXPEDIENTE
-        { name: 'DELETE_RECORD', value: false }, //AGREGAR TRADUCCIÓN ELIMINAR EXPEDIENTE
+        { name: 'UPDATE', value: false },//AGREGAR TRADUCCIÓN ACTUALIZAR EXPEDIENTE
+        { name: 'DELETE', value: false }, //AGREGAR TRADUCCIÓN ELIMINAR EXPEDIENTE
         { name: 'SEND_MESSAGES', value: false}, //AGREGAR TRADUCCIÓN ENVIAR MENSAJES
-        { name: 'SHARE_RECORD', value: false},
-        { name: 'ACCESS_CHILDREN', value: false}]
+        { name: 'SHARE', value: false}]
     ,
     //CAREGIVERS
     "6aef7649-5015-4d71-8d3a-c77979cb5065": [
-        { name: 'CREATE_RECORD', value: false },
+        { name: 'CREATE', value: false },
         { name: 'ACCESS_RECORD', value: false }, //AGREGAR TRADUCCIÓN ACCEDER A EXPEDIENTE
-        { name: 'UPDATE_RECORD', value: false },//AGREGAR TRADUCCIÓN ACTUALIZAR EXPEDIENTE
-        { name: 'DELETE_RECORD', value: false }, //AGREGAR TRADUCCIÓN ELIMINAR EXPEDIENTE
+        { name: 'UPDATE', value: false },//AGREGAR TRADUCCIÓN ACTUALIZAR EXPEDIENTE
+        { name: 'DELETE', value: false }, //AGREGAR TRADUCCIÓN ELIMINAR EXPEDIENTE
         { name: 'SEND_MESSAGES', value: false}, //AGREGAR TRADUCCIÓN ENVIAR MENSAJES
-        { name: 'SHARE_RECORD', value: false},
-        { name: 'ACCESS_CAREGIVERS',value: false}]
+        { name: 'SHARE', value: false}]
     ,
     //APPOINTMENTS
     "2ea0eb1a-391e-4559-80a7-d548b4342b27": [
@@ -33,16 +30,14 @@ export const Permissions = {
         { name: 'ACCESS_PAPER_BIN', value: false }, //AGREGAR TRADUCCIÓN ACCEDER A PAPELERA
         { name: 'ACCESS_SETTINGS', value: false}, //AGREGAR TRADUCCIÓN ACCEDER A AJUSTES
         { name: 'DELETE_EVENT', value: false }, //AGREGAR TRADUCCIÓN ELIMINAR EVENTO
-        { name: 'DELETE_EXCEPTION', value: false},
-        { name: 'ACCESS_APPOINTMENTS'}]//AGREGAR TRADUCCIÓN ELIMINAR EXCEPCIÓN
+        { name: 'DELETE_EXCEPTION', value: false}]//AGREGAR TRADUCCIÓN ELIMINAR EXCEPCIÓN
     ,
     //MESSAGES
     "7fa9ff4e-1ce0-471f-aae7-70736d6525a3": [
         { name: 'SEND', value: false }, //AGREGAR TRADUCCIÓN ENVIAR
         { name: 'ACCESS_INBOX', value: false }, //AGREGAR TRADUCCIÓN ACCEDER A BANDEJA DE ENTRADA
         { name: 'ACCESS_SENT', value: false }, //AGREGAR TRADUCCIÓN ACCEDER A ENVIADOS
-        { name: 'ACCESS_PAPER_BIN', value: false },//AGREGAR TRADUCCIÓN ACCEDER A PAPELARA (REPETIDA)
-        { name: 'ACCESS_MESSAGES',value: false}] 
+        { name: 'ACCESS_PAPER_BIN', value: false }]//AGREGAR TRADUCCIÓN ACCEDER A PAPELARA (REPETIDA)
     ,
     //BILLER
     "ea15aa98-0b71-4ed7-847a-d133f946d6f6": [
@@ -54,11 +49,9 @@ export const Permissions = {
     "a9f83daa-fd8d-11ed-be56-0242ac120007": [
         { name: 'ADD_NEW_USER', value: false },
         { name: 'ACCESS_PERMISSIONS', value: false }, // AGREGAR TRADUCCIÓN ACCEDER PERMISOS
-        { name: 'UPDATE_PERMISSIONS', value: false },// AGREGAR TRADUCCIÓN ACTUALIZAR PERMISOS
-        { name: 'ACCESS_USERS',value: false}] 
+        { name: 'UPDATE_PERMISSIONS', value: false }]// AGREGAR TRADUCCIÓN ACTUALIZAR PERMISOS
     ,
     //ACCOUNT (create-account)
     "f56a63f3-b5f2-429b-8ec8-6026a03edab4": [
-        { name: 'UPDATE_CONFIGURATION', value: false }, // AGREGAR TRADUCCIÓN ACTUALIZAR CONFIGURACION
-        { name: 'ACCESS_ACCOUNT',value: false}]
+        { name: 'UPDATE_CONFIGURATION', value: false }] // AGREGAR TRADUCCIÓN ACTUALIZAR CONFIGURACION
 }
